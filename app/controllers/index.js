54e83b1d3006f26463ecad60737367426fcff6f8
GabrielGbl <gabriel_gbl_05@hotmail.com>
2017-08-17 14:15:56 -0300

    Configuração do express-session, verificação dos campos do login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,21 @@
 module.exports.home = function(application, req, res){
-	res.render('index');
+	res.render('index', {validacao:{}});
+}
+
+module.exports.autenticar = function(application, req, res){
+
+	const dadosForm = req.body;
+
+	req.assert('usu_usuario', "Usuário não pode ser vazio").notEmpty();
+	req.assert('usu_senha', "Senha não pode ser vazio").notEmpty();
+
+	const erros = req.validationErrors();
+
+	if(erros){
+		res.render('index', {validacao:erros});
+		return;
+	}
+
+	res.render('jogo');
+
 }
--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const consign = require('consign');
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
+const expressSession = require('express-session')
 
 
 const app = express();
@@ -9,11 +10,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+
+
 app.use(express.static('./public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(expressValidator());
 
+app.use(expressSession({
+	secret:'oksdaopkdaspkrapoj',
+	resave: false, //CRIA UMA SESSÃO NO SERVIDOR SEMPRE QUE TIVER O REQUEST
+	saveUninitialized:false //CRIA UMA NOVA SESSÃO SEMPRE QUE FOR MODIFICADA
+}));
 
 consign()
 	.include('./app/routes')
